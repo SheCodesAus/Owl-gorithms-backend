6f3d25a9f2cc433e8a29911423e9ca4b5caff16b
KAYCEE LAWRENCE <kaysisdigital@gmail.com>
2026-02-28 12:02:08 +1000

    initial lists model

--- main/lists/models.py
+++ main/lists/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from users.models import User
 
 # Create your models here.
+class List(models.Model):
+    
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    image = models.URLField(null=True, blank=True)
+    is_open = models.BooleanField(default=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    category = models.CharField(max_length=50,
+        choices=[
+            ('one', 'One'),
+            ('two', 'Two'),
+            ('three', 'Three'),
+        ])
+    
+    has_deadline = models.BooleanField(default=False)
+    deadline = models.DateTimeField(null=True, blank=True)
+    
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='lists'
+    )
+    
+    def __str__(self):
+        return f"{self.title}"
