48d1dd441d28de102df4c08c15b8727b4ec75c61
KAYCEE LAWRENCE <kaysisdigital@gmail.com>
2026-02-28 13:00:20 +1000

    removed import buckets

--- main/buckets/views.py
+++ main/buckets/views.py
@@ -3,7 +3,6 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, permissions
 from django.http import Http404
-from .models import Bucket
 from .serializers import BucketSerializer
 from users.models import User
 
