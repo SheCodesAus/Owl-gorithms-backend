b2d655115124a7162139dd84c60c2065972f7223
KAYCEE LAWRENCE <kaysisdigital@gmail.com>
2026-03-03 16:28:16 +1000

    migration file for bucketlists models

--- main/main/settings.py
+++ main/main/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'users.apps.UsersConfig',
+    'bucketlists.apps.BucketlistsConfig',
     'rest_framework',
     'rest_framework.authtoken',
     'django.contrib.admin',
