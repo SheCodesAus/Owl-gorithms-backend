dbfc1f52a4832c694cedba17e24f6ad955d6313a
KAYCEE LAWRENCE <kaysisdigital@gmail.com>
2026-03-09 23:50:26 +1000

    pull profile picture from google account

--- main/users/adapters.py
+++ main/users/adapters.py
@@ -1,4 +1,12 @@
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
 
 class CustomSocialAccountAdapter(DefaultSocialAccountAdapter):
-    pass
+    def save_user(self, request, sociallogin, form=None):
+        user = super().save_user(request, sociallogin, form)
+        
+        picture_url = sociallogin.socialaccount.extra_data.get("picture")
+        if picture_url and user.profile_image != picture_url:
+            user.profile_image = picture_url
+            user.save(update_fields=["profile_image"])
+            
+        return user
--- main/users/models.py
+++ main/users/models.py
@@ -2,6 +2,8 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    profile_image = models.URLField(blank=True, null=True)
     
     
     def __str__(self):
