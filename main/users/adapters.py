ee040b84ac4c066273f60327d91b41d2f6219595
KAYCEE LAWRENCE <kaysisdigital@gmail.com>
2026-03-10 01:35:24 +1000

    update: SocialAccountAdapter - using pre_social_login() instead of save_user()

--- main/users/adapters.py
+++ main/users/adapters.py
@@ -1,30 +1,30 @@
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
 
+
 class CustomSocialAccountAdapter(DefaultSocialAccountAdapter):
-    def save_user(self, request, sociallogin, form=None):
-        user = super().save_user(request, sociallogin, form)
-        
+    def pre_social_login(self, request, sociallogin):
+        user = sociallogin.user
         extra_data = sociallogin.account.extra_data
-        
+
         first_name = extra_data.get("given_name", "")
         last_name = extra_data.get("family_name", "")
         picture_url = extra_data.get("picture")
-        
+
         fields_to_update = []
-        
+
         if first_name and user.first_name != first_name:
             user.first_name = first_name
             fields_to_update.append("first_name")
-            
+
         if last_name and user.last_name != last_name:
             user.last_name = last_name
             fields_to_update.append("last_name")
-       
+
         if picture_url and user.profile_image != picture_url:
             user.profile_image = picture_url
             fields_to_update.append("profile_image")
-        
-        if fields_to_update:
+
+        if user.pk and fields_to_update:
             user.save(update_fields=fields_to_update)
-            
-        return user
+
+        super().pre_social_login(request, sociallogin)
